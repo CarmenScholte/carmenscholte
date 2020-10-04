4be91d2bf5aca893c22218dec4d778ac4c36dd51
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2020-10-04 20:22:05 +0200

    Refactor home, add price to new paintings

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,40 +1,28 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NavigationStart, Router, RouterEvent } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @Input() showMobileNavMenu: boolean;
+  @Input() showMobileNavMenu = false;
+  @Input() theme: string;
   @Output() showMobileNavMenuChange = new EventEmitter<boolean>();
 
-  public showNavigation = true;
   public toggle = false;
   public language: string;
 
-  constructor(
-    private router: Router,
-    public translateService: TranslateService
-  ) {
-    console.log(this.showMobileNavMenu);
+  constructor(public translateService: TranslateService) {
     this.language = 'nl';
-    this.router.events.subscribe((event: RouterEvent) => {
-      if (event instanceof NavigationStart) {
-        if (event.url === '/cv') {
-          this.showNavigation = false;
-        } else {
-          this.showNavigation = true;
-        }
-      }
-    });
   }
 
   ngOnInit(): void {
-    console.log(this.showMobileNavMenu);
+    if (this.theme === 'light') {
+      document.getElementsByTagName('nav')[0].classList.add('header--light');
+    }
   }
+  
 
   public switchLanguage(language: string) {
     this.translateService.use(language);
